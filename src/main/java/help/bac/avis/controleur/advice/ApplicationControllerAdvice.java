e4cc3313f99261936cd0a6c68857601d2ad9cb0b
cabitibaly <bitibalyclementalex@gmail.com>
2024-09-03 21:32:07 +0100

    j'ai géré l'exception liées aux droits

--- src/main/java/help/bac/avis/controleur/advice/ApplicationControllerAdvice.java
+++ src/main/java/help/bac/avis/controleur/advice/ApplicationControllerAdvice.java
@@ -4,14 +4,17 @@ package help.bac.avis.controleur.advice;
 import io.jsonwebtoken.security.MalformedKeyException;
 import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import org.springframework.http.ProblemDetail;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
+
 
 import java.util.Map;
 
@@ -39,10 +42,20 @@ public class ApplicationControllerAdvice {
         return problemDetail;
     }
 
+    @ResponseStatus(FORBIDDEN)
+    @ExceptionHandler(value =  AccessDeniedException.class) // Exception pour les erreurs de connexion
+    public @ResponseBody ProblemDetail badCredentialsException(final AccessDeniedException exception) {
+//        log.error(exception.getMessage(), exception);
+//        ApplicationControllerAdvice.log.error(exception.getMessage(), exception);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(FORBIDDEN, "Vos droits ne vous permettent pas d'accéder à cette ressource");// Explique le probleme qu'on a
+        problemDetail.setProperty("erreur", "Vous n'êtes pas connectés sur le bon compte");
+        return problemDetail;
+    }
+
     @ResponseStatus(UNAUTHORIZED)
     @ExceptionHandler(value = Exception.class)
     public Map<String, String> exceptionHandler() {
-        return Map.of("erreur", "description");
+        return Map.of("erreur", "Quelque s'est mal passé :(");
     }
 
 }
